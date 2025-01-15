e47c56650c8efbe9b532ca9efa69310133459a83
jimmyurl <jimmy.james365@gmail.com>
2025-01-15 10:28:22 +0300

    updated files

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -10,14 +10,14 @@ import 'package:geolocator_android/geolocator_android.dart';
 import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:image_picker_android/image_picker_android.dart';
 import 'package:path_provider_android/path_provider_android.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_android/sqflite_android.dart';
 import 'package:url_launcher_android/url_launcher_android.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:geolocator_apple/geolocator_apple.dart';
 import 'package:google_maps_flutter_ios/google_maps_flutter_ios.dart';
 import 'package:image_picker_ios/image_picker_ios.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_darwin/sqflite_darwin.dart';
 import 'package:url_launcher_ios/url_launcher_ios.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 import 'package:file_selector_linux/file_selector_linux.dart';
@@ -29,8 +29,9 @@ import 'package:file_selector_macos/file_selector_macos.dart';
 import 'package:geolocator_apple/geolocator_apple.dart';
 import 'package:image_picker_macos/image_picker_macos.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_darwin/sqflite_darwin.dart';
 import 'package:url_launcher_macos/url_launcher_macos.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 import 'package:file_selector_windows/file_selector_windows.dart';
 import 'package:image_picker_windows/image_picker_windows.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -80,10 +81,10 @@ class _PluginRegistrant {
       }
 
       try {
-        SqflitePlugin.registerWith();
+        SqfliteAndroid.registerWith();
       } catch (err) {
         print(
-          '`sqflite` threw an error: $err. '
+          '`sqflite_android` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -144,10 +145,10 @@ class _PluginRegistrant {
       }
 
       try {
-        SqflitePlugin.registerWith();
+        SqfliteDarwin.registerWith();
       } catch (err) {
         print(
-          '`sqflite` threw an error: $err. '
+          '`sqflite_darwin` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -254,10 +255,10 @@ class _PluginRegistrant {
       }
 
       try {
-        SqflitePlugin.registerWith();
+        SqfliteDarwin.registerWith();
       } catch (err) {
         print(
-          '`sqflite` threw an error: $err. '
+          '`sqflite_darwin` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -267,6 +268,15 @@ class _PluginRegistrant {
       } catch (err) {
         print(
           '`url_launcher_macos` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        WebKitWebViewPlatform.registerWith();
+      } catch (err) {
+        print(
+          '`webview_flutter_wkwebview` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
