4402f43d4debb6ca65dd21af871a4969b567cb2e
jimmyurl <jimmy.james365@gmail.com>
2025-01-17 22:09:10 +0300

    upgraded

---
+++ build/cloud_firestore/generated/source/buildConfig/debug/com/example/cloud_firestore/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.cloud_firestore;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.cloud_firestore";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String LIBRARY_NAME = "flutter-fire-fst";
+  // Field from default config.
+  public static final String LIBRARY_VERSION = "4.17.5";
+}
---
+++ build/firebase_core/generated/source/buildConfig/debug/com/example/firebase_core/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.firebase_core;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.firebase_core";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String LIBRARY_NAME = "flutter-fire-core";
+  // Field from default config.
+  public static final String LIBRARY_VERSION = "2.32.0";
+}
