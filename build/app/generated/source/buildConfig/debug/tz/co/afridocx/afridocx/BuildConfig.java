40229925e340c4ed76c55f5ba74b2f6c73f9848c
jimmyurl <jimmy.james365@gmail.com>
2025-01-18 13:59:18 +0300

    added files

---
+++ build/app/generated/source/buildConfig/debug/tz/co/afridocx/afridocx/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tz.co.afridocx.afridocx;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tz.co.afridocx.afridocx";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 27;
+  public static final String VERSION_NAME = "2.4.0";
+}
